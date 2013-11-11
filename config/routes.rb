71309d1a815bb8370bc1554492e9fbeaea5834dd
diwei-jiang <shojdw@gmail.com>
2013-11-11 16:33:00 -0500

    diveided into parts

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Djpost::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  root 'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,52 +3,46 @@ require 'spec_helper'
 describe "StaticPages" do
 
   let(:base_title) { "DJiango" }
+  subject { page }
 
   describe "Home page" do
-    it "should have the content 'home'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Home')
-    end
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("DJiango")
-    end
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    before { visit root_path }
+
+    it { should have_content('Welcome') }
+    it { should have_title("DJiango") }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "title should contain base title and page title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About us')
     end
     it "title should contain base title and page title" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About us")
+      visit about_path
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 
   describe "Contact page" do 
     it "should have the content 'Contact us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact us')
     end
     it "title should contain base title and page title" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact us")
+      visit contact_path
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
