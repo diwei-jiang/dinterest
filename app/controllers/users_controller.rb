2b40105b61c568dc6b5a365c59adcbfda22fb87b
diwei-jiang <shojdw@gmail.com>
2013-11-19 21:01:58 -0500

    signin, signout, with cookie

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the djpost!"
       redirect_to @user
     else
       render 'new'
     end
   end
-
-
 
 private
   def user_params
